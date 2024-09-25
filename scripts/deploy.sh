2948877c31907d72cf909228a4dba5ff5014fba5
seonghyeoncho <chosh040@naver.com>
2024-09-26 00:07:32 +0900

    :bricks: [infra] deploy.sh 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-docker pull 313706543169.dkr.ecr.us-east-1.amazonaws.com/kagouniv/back:latest
+# 이미지 풀
+docker pull 313706543169.dkr.ecr.us-east-1.amazonaws.com/kagouniv/back
 
+# 도커 실행
 docker compose up -f docker-compose-prod.yml up -d
