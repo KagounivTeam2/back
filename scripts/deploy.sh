f0651c7900a0143e76d2e2aca93cdd81d541fd6a
seonghyeoncho <chosh040@naver.com>
2024-09-26 13:05:24 +0900

    :bricks: [infra] 시크릿 환경 변수 주입 테스트 2

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # 이미지 풀
-docker pull 313706543169.dkr.ecr.us-east-1.amazonaws.com/kagouniv/back
+docker pull 313706543169.dkr.ecr.us-east-1.amazonaws.com/kagouniv/back:prod
 
 cd /home/ec2-user/back
 
 # 도커 실행
-docker-compose -f docker-compose-prod.yml up -d
+docker-compose -f docker-compose-prod.yml --env-file .env up -d
