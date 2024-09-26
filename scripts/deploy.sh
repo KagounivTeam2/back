a12a490a8ddd952d22bd7d2471e8f8e195130b11
seonghyeoncho <chosh040@naver.com>
2024-09-26 13:37:12 +0900

    :bricks: [infra] 인프라 최종 2

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,4 +6,4 @@ docker pull 313706543169.dkr.ecr.us-east-1.amazonaws.com/kagouniv/back:prod
 cd /home/ec2-user/back
 
 # 도커 실행
-docker-compose -f docker-compose-prod.yml up -d
+docker-compose -f docker-compose-prod.yml --env-file .env up -d
