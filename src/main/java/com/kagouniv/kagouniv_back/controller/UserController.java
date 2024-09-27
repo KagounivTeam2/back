32393edfa50541f273f41346491910cf8df5b18e
sonsumin <sonsumin00@gmail.com>
2024-09-27 15:12:46 +0900

    [#5]Feat: 유저 컨트롤러 작성

--- src/main/java/com/kagouniv/kagouniv_back/controller/UserController.java
+++ src/main/java/com/kagouniv/kagouniv_back/controller/UserController.java
@@ -1,13 +1,38 @@
 package com.kagouniv.kagouniv_back.controller;
 
+import com.kagouniv.kagouniv_back.domain.User;
+import com.kagouniv.kagouniv_back.dto.UserRequest;
+import com.kagouniv.kagouniv_back.dto.response.ResponseDto;
+import com.kagouniv.kagouniv_back.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/user")
 @RequiredArgsConstructor
-@Tag(name="User Controller", description = "User API")
-public class UserController { }
+@Tag(name = "User Controller", description = "User API")
+public class UserController {
+
+    private final UserService userService;
+
+    @Operation(summary = "회원가입", description = "회원가입")
+    @PostMapping("/")
+    public ResponseDto<String> register(@RequestBody @Valid UserRequest userRequest) {
+        String createdId = userService.register(userRequest);
+        return new ResponseDto<>(createdId.toString());
+    }
+
+    @Operation(summary = "로그아웃", description = "로그아웃")
+    @PostMapping("/logout")
+    public ResponseDto<Void> logout(HttpServletRequest request) {
+        userService.logout(request);
+        return new ResponseDto<>(null);
+    }
+}
