f7f0a4cdaa360bf01eff03f3a4ffd276250c941e
sonsumin <sonsumin00@gmail.com>
2024-09-27 15:09:08 +0900

    [#3]Feat: 의존성 추가, 시큐리티 수정

--- src/main/java/com/kagouniv/kagouniv_back/config/SecurityConfig.java
+++ src/main/java/com/kagouniv/kagouniv_back/config/SecurityConfig.java
@@ -1,20 +1,42 @@
 package com.kagouniv.kagouniv_back.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kagouniv.kagouniv_back.auth.JwtAuthenticationEntryPoint;
+import com.kagouniv.kagouniv_back.auth.filter.jwt.JsonUsernamePasswordAuthenticationFilter;
+import com.kagouniv.kagouniv_back.auth.filter.jwt.JwtAuthenticationProcessingFilter;
+import com.kagouniv.kagouniv_back.auth.handler.JwtLoginSuccessHandler;
+import com.kagouniv.kagouniv_back.auth.handler.LoginFailureHandler;
+import com.kagouniv.kagouniv_back.auth.service.JwtService;
+import com.kagouniv.kagouniv_back.dto.response.ResponseDto;
+import com.kagouniv.kagouniv_back.exception.CustomAccessDeniedHandler;
+import com.kagouniv.kagouniv_back.repository.UserRepository;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.LogoutFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final UserDetailsService userDetailsService;
+    private final UserRepository userRepository;
+    private final JwtService jwtService;
+    private final ObjectMapper objectMapper;
+    private final CustomAccessDeniedHandler customAccessDeniedHandler; //커스텀 접근 거부 핸들러
 
     private final String[] allowedUrls = {
             "/swagger-ui/**",
@@ -29,6 +51,7 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
         http.csrf(AbstractHttpConfigurer::disable);
 
         http.formLogin(AbstractHttpConfigurer::disable);
@@ -42,12 +65,73 @@ public class SecurityConfig {
                         sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         http.authorizeHttpRequests(
-                (authorize) ->
-                        authorize
-                                .requestMatchers(allowedUrls)
-                                .permitAll()
-                                .anyRequest().authenticated());
+                        (authorize) ->
+                                authorize
+                                        .requestMatchers("/login").permitAll()
+                                        .requestMatchers("api/user/").permitAll()
+                                        .requestMatchers("/swagger-ui/**", "swagger-resources/**").permitAll()
+                                        .requestMatchers("/v3/api-docs/**").permitAll()
+                                        .anyRequest().authenticated());
+
+                // logout 설정
+        http.logout(logout -> logout.
+                logoutUrl("/api/user/logout")
+//                logoutSuccessUrl("/").
+                .invalidateHttpSession(true)
+                .logoutSuccessHandler((request, response, authentication) -> {
+                    response.setStatus(HttpServletResponse.SC_OK);
+                    response.getWriter().write(new ObjectMapper().writeValueAsString(new ResponseDto<>(null)));
+                    response.getWriter().flush();
+                }));
+
+                // Jwt 관련 설정
+        http.exceptionHandling(req -> req.authenticationEntryPoint(jwtAuthenticationEntryPoint()))
+                .addFilterAfter(jsonUsernamePasswordAuthenticationFilter(), LogoutFilter.class)
+                .addFilterBefore(jwtAuthenticationProcessingFilter(), JsonUsernamePasswordAuthenticationFilter.class)
+
+                // // 커스텀 접근 거부 핸들러 설정
+                .exceptionHandling(exception -> exception
+                        .accessDeniedHandler(customAccessDeniedHandler)
+                );
 
         return http.build();
     }
+
+    @Bean
+    public AuthenticationManager authenticationManager() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder());
+        provider.setUserDetailsService(userDetailsService);
+        return new ProviderManager(provider);
+    }
+
+    @Bean
+    public JwtLoginSuccessHandler jwtLoginSuccessHandler() {
+        return new JwtLoginSuccessHandler(jwtService, userRepository);
+    }
+
+    @Bean
+    public JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint() {
+        return new JwtAuthenticationEntryPoint();
+    }
+
+    @Bean
+    public JwtAuthenticationProcessingFilter jwtAuthenticationProcessingFilter() {
+        return new JwtAuthenticationProcessingFilter(jwtService, userRepository);
+    }
+
+    @Bean
+    public LoginFailureHandler loginFailureHandler() {
+        return new LoginFailureHandler();
+    }
+
+    @Bean
+    public JsonUsernamePasswordAuthenticationFilter jsonUsernamePasswordAuthenticationFilter() throws Exception {
+        JsonUsernamePasswordAuthenticationFilter filter = new JsonUsernamePasswordAuthenticationFilter(objectMapper);
+        filter.setAuthenticationManager(authenticationManager());
+        filter.setAuthenticationSuccessHandler(jwtLoginSuccessHandler());
+        filter.setAuthenticationFailureHandler(loginFailureHandler());
+        return filter;
+    }
+
 }
