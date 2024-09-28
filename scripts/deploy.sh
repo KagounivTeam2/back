8555085fe83010e4a01a057c1e5fedf77a335f50
seonghyeoncho <chosh040@naver.com>
2024-09-28 16:49:27 +0900

    :ambulance: [fix] deploy script 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+$(aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 313706543169.dkr.ecr.us-east-1.amazonaws.com)
 # 이미지 풀
 docker pull 313706543169.dkr.ecr.us-east-1.amazonaws.com/kagouniv/back:prod
 
